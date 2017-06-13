31c59ce30531b893515135e9e421154173cfb783
abhishandy <abhishek.sandy@gmail.com>
2017-06-13 09:04:50 +0530

    carrier and shipper forms refactored

--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -3,4 +3,6 @@ class Carrier < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :trucks
 end
