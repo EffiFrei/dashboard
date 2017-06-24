8e9a781af4f047afe912bf38d7b6904271d454bd
abhishandy <abhishek.sandy@gmail.com>
2017-06-24 19:23:20 +0530

    other fields with validations in two step

--- app/form_models/wizard/request.rb
+++ app/form_models/wizard/request.rb
@@ -15,22 +15,34 @@ module Wizard
 
     class Step1 < Base
       validates :source, length: { minimum: 1 }, presence: true
-      validates :carrier_id, presence: true
+			validates :destination, length: { minimum: 1 }, presence: true
       validates :shipper_id, presence: true
+      validates :num_trucks, presence: true, numericality: { only_integer: true , greater_than: 0 }
     end
 
     class Step2 < Step1
-			validates :destination, length: { minimum: 1 }, presence: true
+			validates :poc_src, length: { minimum: 3 }, presence: true
+      validates :phone_src, length: { is: 10 }, presence: true
+      validates :email_src, presence: true
+
+			validates :poc_dest, length: { minimum: 3 }, presence: true
+      validates :phone_dest, length: { is: 10 }, presence: true
+      validates :email_dest, presence: true
     end
 
     class Step3 < Step2
-			validates :phone_src, length: { is: 10 }, presence: true
-			validates :phone_dest, length: { is: 10 }, presence: true
+      validate :reqd_date_in_the_future
+      validates :service_lvl, presence: true
+
+      def reqd_date_in_the_future
+        if reqd_date.present? && reqd_date < Date.today
+          errors.add(:reqd_date, "can't be in the past")
+        end
+      end
     end
 
     class Step4 < Step3
-			validates :poc_src, length: { minimum: 3 }, presence: true
-			validates :poc_dest, length: { minimum: 3 }, presence: true
+      validates :carrier_id, presence: true
     end
   end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -4,8 +4,22 @@ class Request < ApplicationRecord
 
 	validates :source, length: { minimum: 1 }, presence: true
 	validates :destination, length: { minimum: 1 }, presence: true
+
 	validates :poc_src, length: { minimum: 3 }, presence: true
 	validates :poc_dest, length: { minimum: 3 }, presence: true
 	validates :phone_src, length: { is: 10 }, presence: true
 	validates :phone_dest, length: { is: 10 }, presence: true
+	validates :email_src, presence: true
+	validates :email_dest, presence: true
+	validates :num_trucks, presence: true, numericality: { only_integer: true , greater_than: 0 }
+	validates :service_lvl, presence: true
+	validates :carrier_id, presence: true
+	validates :shipper_id, presence: true
+	validate  :reqd_date_in_the_future
+
+	def reqd_date_in_the_future
+		if reqd_date.present? && reqd_date < Date.today
+			errors.add(:reqd_date, "can't be in the past")
+		end
+	end
 end
