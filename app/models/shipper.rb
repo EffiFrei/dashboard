3bc418ace5e85398e83039f012f503cb2f4d8b6d
abhishandy <abhishek.sandy@gmail.com>
2017-06-25 16:40:25 +0530

    shipper validation

--- app/models/shipper.rb
+++ app/models/shipper.rb
@@ -6,12 +6,12 @@ class Shipper < ApplicationRecord
 
  	has_many :requests
 
-  validates :name, length: { minimum: 3 }, presence: true
-  validates :poc, length: { minimum: 3 }, presence: true
-  validates :phone, length: { is: 10 }, presence: true
-  validates :PAN, length: { is: 10 }, presence: true
-  validates :CIN, length: { is: 21 }, presence: true
+  validates :name, length: { minimum: 3 }, allow_nil: true
+  validates :poc, length: { minimum: 3 }, allow_nil: true
+  validates :phone, length: { is: 10 }, allow_nil: true
+  validates :PAN, length: { is: 10 }, allow_nil: true
+  validates :CIN, length: { is: 21 }, allow_nil: true
   validates :email, presence: true
   validates :password, presence: true
-  validates :password_confirmation, presence: true
+  validates :password_confirmation, presence: true, allow_nil: true
 end
